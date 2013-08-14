076894510c79b8fa0757fb76a34afa9c56402c30
Alby <albytchawula@iinet.net.au>
2013-08-14 16:25:16 +0800

    started q3

---
+++ 104/week3/Fraction.java
@@ -0,0 +1,19 @@
+// Question 3 for ICT104 lab week 3
+// Author: Alby Chawula
+
+public static class Fraction()
+[
+	public int numerator;
+	public int denominator;
+	
+	public static void GetFraction()
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		Systemm
+		this.numerator = keyboard.nextInt();
+		this.denominator = keyboard.nextInt();
+	}
+	
+	
+}
